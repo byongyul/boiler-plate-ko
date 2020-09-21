595454939fad3291b11a8cb7ea29e24d1ec25462
byongyul <byongyul@gmail.com>
2020-09-21 23:53:32 +0900

    webToken 설치 직전 2020 09 21

--- index.js
+++ index.js
@@ -2,14 +2,53 @@ const express  = require("express");
 const app = express();
 const port =3000;
 
+const bodyParser = require("body-parser");
+const {User} = require("./models/User");
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+
 const mongoose =require('mongoose');
 mongoose.connect('mongodb+srv://bychoi:abcd1234@boilerplate.iv8li.mongodb.net/<dbname>?retryWrites=true&w=majority',
                 { userBewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:true
-                }).then(()=>{console.log('MonggoDB connected...')}).catch(err=> console.log(err))
-app.get('/',(req,res)=>{
-    res.send("Hello World 안녕하세요?");
-})
+                }).then(()=>{console.log('MongoDB connected...')}).catch(err=> console.log(err))
 
+app.get('/',(req,res)=>{ res.send("Hello World! Happy Chuseok!");});
+
+app.post('/register',(req,res)=>{
+    const user = new User(req.body);
+
+    user.save((err,userInfo)=>{
+        if(err) return res.json({succes:false,err})
+        return res.status(200).json(
+            {success:true}
+        )
+    });
+});
+
+app.post('/login',()=>{
+    //email check
+    //password check
+    //비밀번호까지 맞다면 토큰 생성
+
+    User.findOne({ emal:req.body.email    },(err,userInfo)=>{
+        if(!userInfo){
+            return res.json(
+                {
+                    loginSuccess:false,
+                    message:"제공된 이메일에 해당하는 유저가 없습니다."
+                }
+            )
+        }
+        user.comparePassword(req.body.password,(err,isMatch)=>{
+            if(!isMatch) return res.json({loginSuccess:false,message:"비밀번호가 틀렸습니다."})
+            user.generateToken((err,user)=>{
+                
+            })
+        })
+    })
+});
 app.listen(port,()=>{
     console.log("server listens on 3000.")
 });
--- models/User.js
+++ models/User.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcrypt");
+const saltRounds = 10
+
 const userSchema = mongoose.Schema({
     name:{
         type:String,
-        maxLength:50
+        maxlength:50
     },
     email:{
         type:String,
         trim:true,
-        unique:1
+    },
+    password:{
+        type:String,
+        minlength:5
     },
     lastname:{
         type:String,
@@ -25,6 +31,28 @@ const userSchema = mongoose.Schema({
         type:Number
     }
 })
+userSchema.pre('save',function(next){
+    var user = this;
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, (err, salt)=> {
+            if(err) return next(err);
+            bcrypt.hash(user.password, salt, (err, hash)=>{
+                if(err) return next(err)
+                user.password = hash;
+                next()
+            });
+        });
+    }else {
+        next();
+    }
+})
 
+userSchema.methods(comparePassword = function(plainPassword,cb){
+    //plainpassword 1234567
+    bcrypt.compare(plainPassword,this.password),(error, isMatch)=>{
+        if(err){ return cb(err)}
+        cb(null,isMatch)
+    }
+})
 const User = mongoose.model('User',userSchema)
 module.exports ={User}
